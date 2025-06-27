f596bfc2d117878a5261b4c5bec2d274ea8d5591
Daniel <danielalderete513@gmail.com>
2025-06-27 16:12:59 -0300

    Trabajo

---
+++ 06 Datos complejos/Datos_Complejos.py
@@ -0,0 +1,8 @@
+'''1) Dado el diccionario precios_frutas 
+precios_frutas = {'Banana': 1200, 'Ananá': 2500, 'Melón': 3000, 'Uva': 
+1450} 
+Añadir las siguientes frutas con sus respectivos precios: 
+● Naranja = 1200 
+● Manzana = 1500 
+● Pera = 2300 '''
+
